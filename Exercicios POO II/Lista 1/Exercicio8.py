eb8a31f72600cfe59ef161c7e6911fe6558a5015
André Amaral Rocco <andre.amaral.rocco@gmail.com>
2022-09-01 11:04:12 -0300

    Exercício 8 atualizado (métodos adicionados)

--- Exercicios POO II/Lista 1/Exercicio8.py
+++ Exercicios POO II/Lista 1/Exercicio8.py
@@ -6,39 +6,79 @@ class Forca:
         self.erros = 0
         self.rodadas = 0
         self.vitoria = False
+        self.finalizou = False
         self.entradas = []
         self.letras_disponiveis = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-        # self.alfabeto = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
-    def iniciar_jogo(self):
-        parte_descoberta = []
+        self.alfabeto = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        
+        # Monta a palavra oculta baseada no tamanho da palavra
+        self.parte_descoberta = []
         for i in range(len(self.palavra)):
-            parte_descoberta.append('_') 
+            self.parte_descoberta.append('_') 
+
+    def display(self):
+        def montar_palavra_descoberta():
+            ...
+        ...
+
+    def status(self):
+        # A função status retorna True se o jogo deve continuar e retorna False se o jogo acabou
+
+        # Confere se o jogador ganhou o jogo
+        if '_' not in self.entradas:
+            self.vitoria = True
+            self.finalizou = True
+            return False # O jogo finalizou
+        
+        # ALTERAR O VALOR DE ERROS
+        elif self.erros == 12:
+            self.vitoria = False
+            self.finalizou = True
+            return False # O jogo finalizou
+        
+        else:
+            return True # O jogo continuou
 
     def rodada(self):
+        # Entrada da letra escolhida na rodada
         letra_escolhida = str(input("LETRA: ")).upper
-        while letra_escolhida not in self.alfabeto:
+        # Confere se a letra está no alfabeto ou se a letra já foi escolhida
+        while (letra_escolhida not in self.alfabeto) or (letra_escolhida not in self.letras_disponiveis):
             print("\nLETRA INVÁLIDA, DIGITE NOVAMENTE: ")
             letra_escolhida = str(input()).upper
         
-        if letra_escolhida not in palavra: # Se a letra não estiver na palavra
-            ...
+        # Determina o resultado da rodada
+        if letra_escolhida not in self.palavra: # Se a letra não estiver na palavra
+            self.erros += 1
+            
         else: # Se a letra estiver na palavra
-            ...
-    
-    def reiniciar_jogo():
-        ...
+            # Substitui os espaços vazios com a letra acertada na lista self.parte_descoberta
+            for index_posicao, letra in enumerate(self.palavra):
+                if letra == letra_escolhida:
+                    self.parte_descoberta[index_posicao] = letra_escolhida
         
+        # Remove a letra escolhida da lista de letras disponíveis
+        self.letras_disponiveis.remove(letra_escolhida)
+        # Acrescenta o contador de rodadas
+        self.rodadas += 1
+
+        return False
+    
+    def reiniciar(self, palavra):
+        # Reinicia a instância executando novamente a classe construtora
+        self.__init__(palavra)
 
 
 def __main__():
     palavra = getpass("Digite a palavra da forca [a digitação é escondida]: ")
     print(palavra)
 
-    while True:
-        ...
+    jogo = Forca(palavra)
 
-
-
+    while status == True:
+        jogo.display()
+        jogo.rodada()
+    
+    jogo.result() 
 
 __main__()
