d829a459b51b63681463fbb02fdebd9f27b63064
André Amaral Rocco <andre.amaral.rocco@gmail.com>
2022-09-01 11:26:00 -0300

    Exercício 8 atualizado (display iniciado)

--- Exercicios POO II/Lista 1/Exercicio8.py
+++ Exercicios POO II/Lista 1/Exercicio8.py
@@ -8,44 +8,39 @@ class Forca:
         self.vitoria = False
         self.finalizou = False
         self.entradas = []
-        self.letras_disponiveis = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-        self.alfabeto = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        self.letras_disponiveis = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        self.alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
         
         # Monta a palavra oculta baseada no tamanho da palavra
         self.parte_descoberta = []
         for i in range(len(self.palavra)):
             self.parte_descoberta.append('_') 
+    
+    def reiniciar(self, palavra):
+        # Reinicia a instância executando novamente a classe construtora
+        self.__init__(palavra)
 
-    def display(self):
-        def montar_palavra_descoberta():
-            ...
-        ...
-
-    def status(self):
-        # A função status retorna True se o jogo deve continuar e retorna False se o jogo acabou
-
-        # Confere se o jogador ganhou o jogo
-        if '_' not in self.entradas:
+    def status(self): # A função status retorna True se o jogo deve continuar e retorna False se o jogo acabou
+        if '_' not in self.parte_descoberta: # Confere se o jogador ganhou o jogo
             self.vitoria = True
             self.finalizou = True
             return False # O jogo finalizou
         
-        # ALTERAR O VALOR DE ERROS
-        elif self.erros == 12:
+        elif self.erros == 12: # ALTERAR O VALOR DE ERROS
             self.vitoria = False
             self.finalizou = True
             return False # O jogo finalizou
         
-        else:
+        else: # No caso do jogo continuar
             return True # O jogo continuou
 
     def rodada(self):
         # Entrada da letra escolhida na rodada
-        letra_escolhida = str(input("LETRA: ")).upper
+        letra_escolhida = str(input("LETRA: ")).upper()
         # Confere se a letra está no alfabeto ou se a letra já foi escolhida
         while (letra_escolhida not in self.alfabeto) or (letra_escolhida not in self.letras_disponiveis):
-            print("\nLETRA INVÁLIDA, DIGITE NOVAMENTE: ")
-            letra_escolhida = str(input()).upper
+            letra_escolhida = str(input("LETRA INVÁLIDA, DIGITE NOVAMENTE: ")).upper
+        self.entradas.append(letra_escolhida)
         
         # Determina o resultado da rodada
         if letra_escolhida not in self.palavra: # Se a letra não estiver na palavra
@@ -61,12 +56,24 @@ class Forca:
         self.letras_disponiveis.remove(letra_escolhida)
         # Acrescenta o contador de rodadas
         self.rodadas += 1
-
-        return False
     
-    def reiniciar(self, palavra):
-        # Reinicia a instância executando novamente a classe construtora
-        self.__init__(palavra)
+
+    def string_parte_descoberta(self): # Formata a parte descoberta até o momento em uma string
+        string = ''
+        for parte in self.parte_descoberta:
+            string += (parte + ' ')
+        
+        return string
+
+    
+    
+    def display(self):
+        def print_desenho_forca():
+            self.rodadas
+
+        print("\nRODADA: {}".format(self.rodadas))
+        #print_desenho_forca()
+        print(self.string_parte_descoberta())
 
 
 def __main__():
@@ -75,10 +82,12 @@ def __main__():
 
     jogo = Forca(palavra)
 
+    status = True
     while status == True:
         jogo.display()
         jogo.rodada()
+        status = jogo.status()
     
-    jogo.result() 
+    jogo.result()
 
 __main__()
