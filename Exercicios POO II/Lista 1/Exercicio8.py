60852e55d7eff19e8b57f4441c3fbbdf1edacfba
André Amaral Rocco <andre.amaral.rocco@gmail.com>
2022-09-01 10:35:26 -0300

    Exercício 8 atualizado

--- Exercicios POO II/Lista 1/Exercicio8.py
+++ Exercicios POO II/Lista 1/Exercicio8.py
@@ -1,14 +1,30 @@
-from getpass import getpass
+from getpass import getpass # Usado para receber a palavra escolhida sem que ela seja mostrada no console
 
 class Forca:
     def __init__(self, palavra):
-        self.palavra = palavra.upper()
+        self.palavra = [*palavra.upper()] # Transforma a palavra em uma lista com as letras
         self.erros = 0
+        self.rodadas = 0
+        self.vitoria = False
         self.entradas = []
-        self.letras_disponiveis = []
+        self.letras_disponiveis = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        # self.alfabeto = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    def iniciar_jogo(self):
+        parte_descoberta = []
+        for i in range(len(self.palavra)):
+            parte_descoberta.append('_') 
+
+    def rodada(self):
+        letra_escolhida = str(input("LETRA: ")).upper
+        while letra_escolhida not in self.alfabeto:
+            print("\nLETRA INVÁLIDA, DIGITE NOVAMENTE: ")
+            letra_escolhida = str(input()).upper
         
-    def rodada():
-        ...
+        if letra_escolhida not in palavra: # Se a letra não estiver na palavra
+            ...
+        else: # Se a letra estiver na palavra
+            ...
     
     def reiniciar_jogo():
         ...
