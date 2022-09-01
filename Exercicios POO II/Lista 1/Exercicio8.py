7ff73fdf3871bd8dfec5cf1cecfc495e86fa337a
André Amaral Rocco <andre.amaral.rocco@gmail.com>
2022-09-01 10:23:37 -0300

    Exercício 8 atualizado

---
+++ Exercicios POO II/Lista 1/Exercicio8.py
@@ -0,0 +1,28 @@
+from getpass import getpass
+
+class Forca:
+    def __init__(self, palavra):
+        self.palavra = palavra.upper()
+        self.erros = 0
+        self.entradas = []
+        self.letras_disponiveis = []
+        
+    def rodada():
+        ...
+    
+    def reiniciar_jogo():
+        ...
+        
+
+
+def __main__():
+    palavra = getpass("Digite a palavra da forca [a digitação é escondida]: ")
+    print(palavra)
+
+    while True:
+        ...
+
+
+
+
+__main__()
