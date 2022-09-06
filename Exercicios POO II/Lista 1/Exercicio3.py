56eff1aa589a921bfd498c2ee6c532d91ccab4d5
André Amaral Rocco <andre.amaral.rocco@gmail.com>
2022-09-06 10:11:56 -0300

    Exercício 3 atualizado

--- Exercicios POO II/Lista 1/Exercicio3.py
+++ Exercicios POO II/Lista 1/Exercicio3.py
@@ -4,26 +4,36 @@ from math import ceil
 class Polinomio:
     def __init__(self, lista_coeficientes):
         self.lista_coeficientes = lista_coeficientes
+        self.grau = len(lista_coeficientes) - 1
     
     def string_polinomio(self):
         string_polinomio = ''
         for expoente, coeficiente in enumerate(self.lista_coeficientes):
-            
-            # Confere se é negativo ou positivo ou se é o ultimo coeficiente do polinômio
-            if coeficiente >= 0:
-                string_polinomio += ' + '
-            elif coeficiente < 0:
-                string_polinomio += ' - '
-            
+            string_polinomio += ' + '
+
             # Confere se o float é inteiro
             if ceil(coeficiente) == coeficiente:
                 string_polinomio += '{} . x^{}'.format(int(coeficiente), expoente)
             else:
                 string_polinomio += '{} . x^{}'.format(str(coeficiente).replace('.',','), expoente)
 
-
         return string_polinomio
     
+    def get_grau(self):
+        return self.grau
+    
+    def somar_polinomio(self, outro_polinomio):
+        ...
+    
+    def multiplicar_polinomio(self, outro_polinomio):
+        ...
+    
+    def avaliar_resultado(self, x):
+        resultado = 0
+        for expoente, coeficiente in enumerate(self.lista_coeficientes):
+            resultado += coeficiente * (x**expoente) 
+
+        return resultado   
 
 contador = 0
 lista_coeficientes = []
@@ -39,7 +49,9 @@ while True:
             contador += 1
         except ValueError:
             print('Inválido, digite novamente')
-        
+
 
 p1 = Polinomio(lista_coeficientes)
 print(p1.string_polinomio())
+print(p1.get_grau())
+print(p1.avaliar_resultado(4))
