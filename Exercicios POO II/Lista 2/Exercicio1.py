347d2baa4391ae90d034772646ee9a3a61c8c0b5
André Amaral Rocco <andre.amaral.rocco@gmail.com>
2022-09-09 14:06:01 -0300

    Exercício 1 finalizado

--- Exercicios POO II/Lista 2/Exercicio1.py
+++ Exercicios POO II/Lista 2/Exercicio1.py
@@ -1,5 +1,5 @@
 vetor = []
 print("Adicione 5 inteiros para o vetor")
 for i in range(5):
-    vetor.append(int(input(f"{i}. Digite o inteiro: ")))
+    vetor.append(int(input(f"{i+1}. Digite o inteiro: ")))
 print(vetor)
