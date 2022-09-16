3019e889e01563c43d02444ec5b5c75d6da01c2b
André Amaral Rocco <andre.amaral.rocco@gmail.com>
2022-09-16 09:26:16 -0300

    Exercício 8 finalizado

--- Exercicios POO II/Lista 2/Exercicio8.py
+++ Exercicios POO II/Lista 2/Exercicio8.py
@@ -5,5 +5,5 @@ for i in range(5):
     idades.append(int(input("Digite a idade: ")))
     alturas.append(float(input("Digite a altura: ")))
 
-print("Idades: {}".format(idades[::-1]))
-print("Alturas: {}".format(alturas[::-1]))
+print("Idades: {}".format(", ".join([str(x) for x in idades[::-1]])))
+print("Alturas: {}".format(", ".join([str(x) for x in alturas[::-1]])))
