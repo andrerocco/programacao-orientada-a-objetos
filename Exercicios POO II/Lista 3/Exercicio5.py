d5027ac711a5c06ffce5a2784f60edb519ab3bef
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-09-20 18:54:28 -0300

    Exercício 5 finalizado

--- Exercicios POO II/Lista 3/Exercicio5.py
+++ Exercicios POO II/Lista 3/Exercicio5.py
@@ -13,3 +13,46 @@ agenda.
 excluir_nome – essa função exclui uma pessoa da agenda.
 consultar_telefone – essa função retorna os telefones de uma pessoa
 na agenda. """
+
+agenda_telefonica = {}
+
+def incluir_novo_nome(nome, telefones):
+    for telefone in telefones:
+        if nome in agenda_telefonica:
+            agenda_telefonica[nome].append(telefone)
+        else:
+            agenda_telefonica[nome] = [telefone]
+
+def incluir_telefone(nome, telefone):
+    if nome in agenda_telefonica:
+        agenda_telefonica[nome].append(telefone)
+    else:
+        print("Nome não encontrado na agenda!")
+        resposta = input("Deseja incluí-lo? (S/N): ")
+        if resposta == "S":
+            incluir_novo_nome(nome, [telefone])
+
+def excluir_nome(nome):
+    if nome in agenda_telefonica:
+        del agenda_telefonica[nome]
+    else:
+        print("Nome não encontrado na agenda!")
+
+def excluir_telefone(nome, telefone):
+    if nome in agenda_telefonica:
+        if telefone in agenda_telefonica[nome]:
+            agenda_telefonica[nome].remove(telefone)
+            if len(agenda_telefonica[nome]) == 0:
+                excluir_nome(nome)
+        else:
+            print("Telefone não encontrado na agenda!")
+    else:
+        print("Nome não encontrado na agenda!")
+
+def consultar_telefone(nome):
+    if nome in agenda_telefonica:
+        print("Telefones de {}:".format(nome))
+        for telefone in agenda_telefonica[nome]:
+            print(telefone)
+    else:
+        print("Nome não encontrado na agenda!")
