7819444bcf06951d54517c9bdc1d350d39015836
André Amaral Rocco <andre.amaral.rocco@gmail.com>
2022-09-27 11:00:25 -0300

    Construídas classes do sistema

--- Exercicios POO II/VPL 1/cliente.py
+++ Exercicios POO II/VPL 1/cliente.py
@@ -1,9 +1,22 @@
 class Cliente:
-
     """Insira aqui o construtor"""
+    def __init__(self, nome, telefone):
+        self.__nome = nome
+        self.__fone = telefone
 
+    """Insira aqui todos metodos e atributos ... """
     @property
     def nome(self):
         return self.__nome
 
-    """Insira aqui os demais metodos ... """
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @property
+    def fone(self):
+        return self.__fone
+
+    @fone.setter
+    def fone(self, telefone):
+        self.__fone = telefone
--- Exercicios POO II/VPL 1/produto.py
+++ Exercicios POO II/VPL 1/produto.py
@@ -1,21 +1,60 @@
 class Produto:
-    def __init__(self, nome, telefone):
-        self.__nome = nome
-        self.__fone = telefone
-
+    def __init__(self, codigo, descricao, categoria):
+        self.__codigo = codigo
+        self.__descricao = descricao
+        self.__categoria = categoria
+        self.__quantidade = 0
+        self.__preco_unitario = 0.0
+        self.__cliente = None
+    
     """Insira aqui todos metodos e atributos ... """
     @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome
-
+    def codigo(self):
+        return self.__codigo
+    
+    @codigo.setter
+    def codigo(self, codigo):
+        self.__codigo = codigo
+    
     @property
-    def telefone(self):
-        return self.__fone
-
-    @telefone.setter
-    def telefone(self, telefone):
-        self.__fone = telefone
+    def descricao(self):
+        return self.__descricao
+    
+    @descricao.setter
+    def descricao(self, descricao):
+        self.__descricao = descricao
+    
+    @property
+    def categoria(self):
+        return self.__categoria
+    
+    @categoria.setter
+    def categoria(self, categoria):
+        self.__categoria = categoria
+    
+    @property
+    def quantidade(self):
+        return self.__quantidade
+    
+    @quantidade.setter
+    def quantidade(self, quantidade):
+        self.__quantidade = quantidade
+    
+    @property
+    def preco_unitario(self):
+        return self.__preco_unitario
+    
+    @preco_unitario.setter
+    def preco_unitario(self, preco_unitario):
+        self.__preco_unitario = preco_unitario
+    
+    @property
+    def cliente(self):
+        return self.__cliente
+    
+    @cliente.setter
+    def cliente(self, cliente):
+        self.__cliente = cliente
+    
+    def preco_total(self):
+        return self.__quantidade * self.__preco_unitario
