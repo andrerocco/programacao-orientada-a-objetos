5e22213bfcfad59fe5b355317ce2526999ce2f3e
André Amaral Rocco <andre.amaral.rocco@gmail.com>
2022-10-04 09:17:23 -0300

    Correção dos argumentos

--- Exercicios POO II/Sistema de Transporte/TransporteAquatico.py
+++ Exercicios POO II/Sistema de Transporte/TransporteAquatico.py
@@ -2,6 +2,6 @@ from socket import BDADDR_LOCAL
 from Transporte import Transporte
 
 class TransporteAquatico(Transporte):
-    def __init__(self):
+    def __init__(self, boca, calado):
         self.__boca = boca
         self.__calado = calado
