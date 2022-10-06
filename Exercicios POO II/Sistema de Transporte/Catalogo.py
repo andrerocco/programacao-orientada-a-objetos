3f7f607cf01648f64ea842d600de5d801ba6f597
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-10-06 19:18:45 -0300

    Implementado catálogo

--- Exercicios POO II/Sistema de Transporte/Catalogo.py
+++ Exercicios POO II/Sistema de Transporte/Catalogo.py
@@ -1,3 +1,20 @@
+from TransporteAereo import TransporteAereo
+from TransporteAquatico import TransporteAquatico
+from TransporteTerrestre import TransporteTerrestre
+
 class Catalogo:
     def __init__(self):
-        pass
+        self.__veiculos_cadastrados = []
+    
+    def inserir_transporte(self, veiculo):
+        if not isinstance(veiculo, (TransporteAereo, TransporteAquatico, TransporteTerrestre)):
+            return "Tipo de veículo não aceito"
+        else:
+            self.__veiculos_cadastrados.append(veiculo)
+        
+    def mostrar_catalogo(self):
+        for veiculo in self.__veiculos_cadastrados:
+            print(veiculo)
+
+    
+    
